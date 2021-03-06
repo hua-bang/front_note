ad716d84fdd8cce36b5cfa2d94e75779eb31ac83
hua-bang <1964760211@qq.com>
2021-03-06 09:34:22 +0800

    startsWith, endsWith, template string

---
+++ js_base/Es6/String/README.md
@@ -0,0 +1,5 @@
+# String字符串
+####新增特性
++ ##### startsWith
++ ##### endsWith
++ ##### 模板字符串 ``模板字符串(template string 用`标识)
---
+++ js_base/Es6/String/index.js
@@ -0,0 +1,12 @@
+/**
+ * @author hug
+ * @date 2021/3/6 9:29
+ */
+let url = "https://hua";
+console.log(url.startsWith("http"));
+
+console.log(url.endsWith(".com"));
+
+let a = 2;
+// 模板字符串(template string 用`标识)
+console.log(`a的值为${a}`);
