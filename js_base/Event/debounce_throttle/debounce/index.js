5363f379f450483b64977ea6a0d459d42cf1ae32
hua-bang <1964760211@qq.com>
2021-03-20 00:40:23 +0800

    例子加以结合

--- js_base/Event/debounce_throttle/debounce/index.js
+++ js_base/Event/debounce_throttle/debounce/index.js
@@ -1,6 +1,11 @@
 import { debounceImmediate as debounce } from "./debounce.js";
 
-const input = document.querySelector("#input");
-input.addEventListener("input", debounce(() => {
+const inputs = document.querySelectorAll("input");
+
+inputs[0].addEventListener("input", debounce(() => {
+    console.log("input");
+},500, true));
+
+inputs[1].addEventListener("input", debounce(() => {
     console.log("input");
 },500));
