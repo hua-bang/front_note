b993ebb3dbe7647302653e2031888ae8aabae9e9
hua-bang <1964760211@qq.com>
2021-03-30 17:02:11 +0800

    [Symbol.toPrimitive] > valueOf > toString

--- js_base/subject/Object/typeTransformation.js
+++ js_base/subject/Object/typeTransformation.js
@@ -1,15 +1,23 @@
 let a = {
     num: 0,
+    words: "",
     valueOf() {
         return ++this.num;
     },
     toString() {
+        return this.words + "a";
+    },
+    [Symbol.toPrimitive]() {
         return ++this.num;
     }
 }
 
 // 从中可以看出，原始值达不到要求
-// 引用值的话会先看valueOf 再看toString
+// 引用值的话 会先看对象中有没有[Symbol.toPrimitive]属性，再先看valueOf 再看toString
 if(a == 1 && a == 2 && a == 3) {
     console.log("ok");
+}
+
+if(a == "a") {
+    console.log("string-ok");
 }
