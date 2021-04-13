235c8a70e7bbfec7d5ec25ab37c4c73adcd3d510
hua-bang <1964760211@qq.com>
2021-04-13 23:28:13 +0800

    算法

---
+++ datastructures-algorithms/Practice/code/Base/BubbleSort.js
@@ -0,0 +1,15 @@
+function bubbleSort(arr) {
+    let length = arr.length;
+    for (let i = 0; i < length - 1; i++) {
+        for (let j = 0; j < length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort([3, 2, 65, 12, 1000, 2000, 200, 400, 6000]))
