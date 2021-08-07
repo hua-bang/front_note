fcf78a794c3705bbc6e934f609a6c5a219b235c4
hua-bang <1964760211@qq.com>
2021-08-07 15:46:51 +0800

    feated: webpack_dev_ts

---
+++ TypeScript/code/learn_ts/webpack_dev/src/index.ts
@@ -0,0 +1,10 @@
+import { add } from "./add";
+console.log("hello, typescript!!!");
+
+let a = 2;
+console.log(a);
+console.log(add(2, 3));
+
+const b = { name: "hug" };
+b.name = "test";
+console.log(b);
