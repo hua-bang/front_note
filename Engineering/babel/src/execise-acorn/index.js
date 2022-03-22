5b6f7872392bf2604c50e9e0d7c7a276071c99d8
hua-bang <47221942+hua-bang@users.noreply.github.com>
2022-03-22 13:39:43 +0800

    feat: acorn

---
+++ Engineering/babel/src/execise-acorn/index.js
@@ -0,0 +1,48 @@
+const acorn = require("acorn");
+
+const Parser = acorn.Parser;
+const TokenType = acorn.TokenType;
+
+Parser.acorn.keywordTypes["guang"] = new TokenType("guang",{keyword: "guang"});
+
+function wordsRegexp(words) {
+  return new RegExp("^(?:" + words.replace(/ /g, "|") + ")$")
+}
+
+var guangKeyword = function(Parser) {
+  return class extends Parser {
+    parse(program) {
+      let newKeywords = "break case catch continue debugger default do else finally for function if return switch throw try var while with null true false instanceof typeof void delete new in this const class extends export import super";
+      newKeywords += " guang";
+      this.keywords = new RegExp("^(?:" + newKeywords.replace(/ /g, "|") + ")$")
+      return(super.parse(program));
+    }
+
+    parseStatement(context, topLevel, exports) {
+      var starttype = this.type;
+
+      if (starttype == Parser.acorn.keywordTypes["guang"]) {
+        var node = this.startNode();
+        return this.parseGuangStatement(node);
+      }
+      else {
+        return(super.parseStatement(context, topLevel, exports));
+      }
+    }
+
+    parseGuangStatement(node) {
+      this.next();
+      return this.finishNode({value: 'guang'},'GuangStatement');//新增加的ssh语句
+    };
+  }
+}
+const newParser = Parser.extend(guangKeyword);
+
+var program = 
+`
+    guang
+    const a = 1
+`;
+
+const ast = newParser.parse(program);
+console.log(ast);
