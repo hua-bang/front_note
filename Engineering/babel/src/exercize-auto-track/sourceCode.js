1425d858ae06fc315182d6a3d4eb2c4dedb43540
hua-bang <47221942+hua-bang@users.noreply.github.com>
2022-04-01 17:32:34 +0800

    ✨ feat(babel): auto track code

---
+++ Engineering/babel/src/exercize-auto-track/sourceCode.js
@@ -0,0 +1,17 @@
+import 'dd';
+
+function a () {
+  console.log('aaa');
+}
+
+class B {
+  bb() {
+    return 'bbb';
+  }
+}
+
+const c = () => 'ccc';
+
+const d = function () {
+  console.log('ddd');
+}
